4ee752e68807edd54c677bb888ce94daa0ca418b
rhys909 <rhyskconnor99@gmail.com>
2019-04-11 21:58:27 +0100

    case 4 + 5 done

--- Coursework_1/Q1/PlayerStore.java
+++ Coursework_1/Q1/PlayerStore.java
@@ -3,6 +3,7 @@ package q1java;
 import java.util.Scanner;
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 
 public class PlayerStore{
   public static void main(String args[]){
@@ -25,7 +26,7 @@ public class PlayerStore{
       System.out.println("7) Delete an entry from the dictionary");
       System.out.println("8) Search for a Stadium");
       System.out.println("9) Enter the range of players you wish to view i.e. 2-6");
-      System.out.println("10) EXIT")
+      System.out.println("10) EXIT");
       System.out.println("Please enter your choice: ");
 
       int userChoice = numGet.nextInt();
@@ -39,7 +40,7 @@ public class PlayerStore{
               Hashtable<String, String> RugbyPlayersTable = new Hashtable<String, String>();
             }
             catch(Exception e){
-              System.out.println("Error generating dictionary.")
+              System.out.println("Error generating dictionary.");
             }
             break;
 
@@ -68,30 +69,30 @@ public class PlayerStore{
                 RugbyPlayer player = new RugbyPlayer(Name,playerID,triesScored,teamName,teamID,stdmName,stdmTown,stdmPstCde,stdmStreet);
               }
               catch(Exception e){
-                System.out.println("Player was not successfully created")
+                System.out.println("Player was not successfully created");
               }
               //add the player to the hash map
-              RugbyPlayersTable.put(playerID, player.playerToString())
+              RugbyPlayersTable.put(playerID, player.playerToString());
             }
             catch(Exception e){
-              System.out.println("Error adding the player to dictionary")
+              System.out.println("Error adding the player to dictionary");
             }
             break;
 
           case 3:
             try {
-              System.out.println("Please enter the name of the file you wish to read:\n")
-              String filename = textGet.nextString() + ".txt"
+              System.out.println("Please enter the name of the file you wish to read:\n");
+              String filename = textGet.nextString() + ".txt";
               file = load(true, filename);
             }
             catch(Exception e){
-              System.out.println("Unable to load .txt file")
+              System.out.println("Unable to load .txt file");
             }
             break;
 
           case 4:
             try {
-              System.out.println("Please enter a name for your new file:\n")
+              System.out.println("Please enter a name for your new file:\n");
               String newFile = textGet.nextString() + ".txt";
               //generate the new file for the user
               File file = new File("/" + newFile);
@@ -100,12 +101,36 @@ public class PlayerStore{
             }
             catch(IOException e){
               e.printStackTrace();
-              System.out.println("Error creating a blank .txt file")
+              System.out.println("Error creating a blank .txt file");
             }
             break;
           case 5:
             try {
-
+              System.out.println("Would you like to view the players in the .txt file or the dictionary? \nPlease enter 1 for .txt and 2 for dictionary.");
+              int choice = numGet.nextInt();
+              if(choice == 1){
+                System.out.println("Please enter the name of the file which you wish to read:");
+                String filenameRead = textGet.nextString();
+                File file = new File(filenameRead);
+                int i = 0;
+                BufferedReader br = new BufferedReader(new FileReader(file));
+                String line;
+                int len = 0;
+                while((line = br.readLine()) != null) {
+                  System.out.println(line);
+              } else if (choice == 2){
+                if (RugbyPlayersTable.size() == 0){
+                  System.out.println("Please add some players to the dictionary");
+                } else if(RugbyPlayersTable.size() >= 1){
+                  for (RugbyPlayersTable.Entry<String , String> entry : RugbyPlayersTable.entrySet() ) {
+                    System.out.println(entry.getValue());
+                  } else{
+                    System.out.println("Please create the dictionary of rugby players!")
+                  }
+                }
+              } else{
+                System.out.println("Error please enter a valid option!");
+              }
             }
             catch(Exception e){
 
@@ -151,5 +176,9 @@ public class PlayerStore{
 
             }
             break;
+        }
     }
-}}
+
+
+}
+}
