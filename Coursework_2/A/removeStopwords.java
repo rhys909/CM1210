e6922a8a41b03c904a4269e0b961b7e7a6e3239c
rhys909 <rhyskconnor99@gmail.com>
2019-04-30 12:28:25 +0100

    Question A done

--- Coursework_2/A/removeStopwords.java
+++ Coursework_2/A/removeStopwords.java
@@ -2,10 +2,29 @@ import java.util.*;
 import java.io.*;
 
 public class removeStopwords{
-  public static void main ( args [] ){
-    Scanner txt = new Scanner(System.in);
-    ArrayList<String> Stopwords = new ArrayList<String>();
-    ArrayList<String> GPT2 = new ArrayList<String>();
-    System.out.println()
+  public static void main (String[] args ){
+
+    ArrayList<String> StopwordsArray = new ArrayList<String>();
+    ArrayList<String> GPT2Array = new ArrayList<String>();
+    String stopwordsFile = "stopwords.txt";
+    String GPT2Filename = "GPT2.txt";
+
+    try {
+      Scanner stopFile = new Scanner(new File(stopwordsFile));
+      Scanner GPT2File = new Scanner(new File(GPT2Filename));
+      while(stopFile.hasNext()){
+        StopwordsArray.add(stopFile.next());
+      }
+      stopFile.close();
+      while(GPT2File.hasNext()){
+        GPT2Array.add(GPT2File.next());
+      }
+      GPT2File.close();
+    } catch( IOException e ){
+      System.out.println("File Error!");
+    }
+
+    GPT2Array.removeAll(StopwordsArray);
+    System.out.println(GPT2Array);
   }
 }
