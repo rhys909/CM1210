a5dea6fcf3ea59767e23cf753de42053fe46e89f
Rhys909 <rhyskconnor99@gmail.com>
2019-04-30 10:06:58 +0100

    .txt files

--- Coursework_2/A/removeStopwords.java
+++ Coursework_2/A/removeStopwords.java
@@ -1,5 +1,11 @@
+import java.util.*;
+import java.io.*;
+
 public class removeStopwords{
   public static void main ( args [] ){
-    
+    Scanner txt = new Scanner(System.in);
+    ArrayList<String> Stopwords = new ArrayList<String>();
+    ArrayList<String> GPT2 = new ArrayList<String>();
+    System.out.println()
   }
 }
